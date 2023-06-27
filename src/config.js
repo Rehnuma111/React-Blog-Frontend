0bef1cbfabfc081d9d71882233773b2212550ac6
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-06-27 21:25:28 +0530

    backend link added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const app_config = {
-    app_url:"http://localhost:8000"
-}
+  // app_url:"http://localhost:8000"
+  app_url: "https://rehnuma-react-blog-backend.onrender.com",
+};
 export default app_config;
